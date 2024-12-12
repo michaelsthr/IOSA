d72dff7e39da54a6f03e86def4c4b24a49ad57d6
matthias.steger <matthias.stgr@gmail.com>
2024-12-12 11:47:04 +0100

    Added a polymorph Proces structure -> not finished

--- process.py
+++ process.py
@@ -1,5 +1,5 @@
 class Process:
-    def __init__(self,name=None, ready_time=None, exec_time=None, deadline=None, start_time=None, complet_time=None):
+    def __init__(self, name=None, ready_time=None, exec_time=None, deadline=None, start_time=None, complet_time=None):
         self.name = name
         self.ready_time = ready_time
         self.exec_time = exec_time
@@ -12,3 +12,21 @@ class Process:
         return "".join(f"{key}: {value}\n"
             for key, value in vars(self).items() if value is not None
         )
+    
+class FCFS_Process(Process):
+    pass
+
+class EDF_Process(Process):
+    def __init__(self, exec_time, deadline, name=None):
+        pass
+
+class LLF_Process(Process):
+    def __init__(self, ready_time, exec_time, deadline, name=None):
+        pass
+
+class SJF_Process(Process):
+    pass
+
+class RoundRobin_Process(Process):
+    def __init__(self, exec_time, left_exec_time, name=None):
+        pass
