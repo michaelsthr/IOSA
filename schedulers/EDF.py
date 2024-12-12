d7c132209bb9f6954f26ac2965ed2647971c2946
Sammweis <sammweis.gamtschi@gmail.com>
2024-12-12 08:18:48 +0000

    finished EDF algo

--- schedulers/EDF.py
+++ schedulers/EDF.py
@@ -1,32 +1,31 @@
 from process import Process
 
-edf_list = [Process(deadline=9, exec_time=4),
+raw_edf_list = [Process(deadline=9, exec_time=4),
              Process(deadline=9, exec_time=5),
              Process(deadline=10, exec_time=8)]
 
-def Schedule() -> float:
-    print("Earliest deadline first!\n")
+
+def schedule() -> float:
+    print('First Come First Serf:\n')
     print('Processes:\nExecution-Times:')
 
+    # sorted list only by deadlines, not by execution times
+    edf_list = raw_edf_list.sort(key=lambda process: process.deadline)
+    
     for idx in range(len(edf_list)):
         print('  Execution time process', (idx+1), ':', edf_list[idx].exec_time)
-    
-    added_exe_time = 0
-    is_even_processes = (len(edf_list)/2 == 0)
+
+
+    ave_waiting_time = 0
+    added_waiting_time = 0
+    rel_waiting_time = 0
     for i in edf_list:
-        # if ((i == edf_list[0]) or (i == edf_list[1])):
-        #     added_exe_time += i.exec_time
-        # if (i == edf_list[-1]):
-        #     avg_waiting_time = added_exe_time / len(edf_list)
-        if is_even_processes:
-            if (i != edf_list[-2]):
-                added_exe_time += i.exec_time
-            else:
-                avg_waiting_time = added_exe_time / len(edf_list)
-                return avg_waiting_time
-        else:
-            if (i != edf_list[-1]):
-                added_exe_time += i.exec_time
-            else:
-                avg_waiting_time = added_exe_time / len(edf_list)
-                return avg_waiting_time
+        rel_waiting_time = rel_waiting_time + i.exec_time
+        added_waiting_time += rel_waiting_time
+        print('+', rel_waiting_time)
+        
+    print(added_waiting_time, ' / ', len(edf_list))
+    ave_waiting_time = added_waiting_time / len(edf_list)
+    print('Average waiting time:', ave_waiting_time)
+    print('='*40)
+    return ave_waiting_time
