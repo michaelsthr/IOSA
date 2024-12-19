9f2aba9cafd6e27e0d96dd42157d782a0ceff2d5
michisthr <michlstoehr19@icloud.com>
2024-12-19 01:04:43 +0100

    add terminal input

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
 from schedulers.sjf import ShortestJobFirst
+from schedulers.llf import LeastLaxityFirst
+from schedulers.fcfs import FirstComeFirstServe
+from schedulers.edf import EarliestDeadlineFirst
+
+from colorama import Fore, Style
+from art import tprint
 
 if __name__ == "__main__":
-    print("=" * 40)
-    print("Welcome to IOSA - Implementation Of Scheduling Algorithms")
-    print("Made by: Matthi, Samu, Michi")
-    print("=" * 40)
-    
-    sjf = ShortestJobFirst()
-    sjf.schedule()
-    sjf.plot()
+    tprint("IOSA")
+    print("-" * 40)
+    print(f"{Fore.CYAN}Welcome to IOSA - Implementation Of Scheduling Algorithms{Fore.RESET}")
+    print(f"{Fore.GREEN}Made by: Matthi, Samu, Michi{Fore.RESET}")
+    print("-" * 40)
+
+    # TODO: Multiple windows at once
+
+    def llf():
+        llf = LeastLaxityFirst()
+        llf.schedule()
+        llf.plot()
+
+    def edf():
+        edf = EarliestDeadlineFirst()
+        edf.schedule()
+        edf.plot()
+
+    def sjf():
+        sjf = ShortestJobFirst()
+        sjf.schedule()
+        sjf.plot()
+
+    def fcfs():
+        fcfs = FirstComeFirstServe()
+        fcfs.schedule()
+        fcfs.plot()
+
+    def rr():
+        # TODO
+        pass
+
+    while True:
+        try:
+            schedulers = ["sjf", "fcfs", "edf", "llf", "rr"]
+            print("\n")
+            print("-" * 40, end="\n")
+            user_input = input(f"Paste your desired scheduler {schedulers}\n"
+                               "Type exit to exit :o\n"
+                               " --> ").strip()
+            if user_input in schedulers:
+                exec(f"{user_input}()")
+            elif user_input == "exit":
+                exit()
+            else:
+                print("Invalid scheduler!")
+        except Exception as ex:
+            print(f"{Fore.YELLOW} An unexpected error occured: {ex}\n"
+                  f"But you can try other schedulers :){Fore.RESET}\n")
