ec4ae9b02c8f961148a72524c54025dff944eb5a
michisthr <michlstoehr19@icloud.com>
2024-12-19 11:07:16 +0100

    refacor RoundRobin schedule()

--- schedulers/rr.py
+++ schedulers/rr.py
@@ -20,25 +20,25 @@ class RoundRobin:
         clock = 0
         sum_finish_time = 0
         while copy_list:
-            len_copy_list = len(copy_list)
-            for i in range(len_copy_list):
-                if (copy_list[i].left_exec_time > quantum):
-                    copy_list[i].left_exec_time -= quantum
-                    scheduled.append((copy_list[i].name, quantum))
+            for process in copy_list:
+                if (process.left_exec_time > quantum):
+                    process.left_exec_time -= quantum
+                    scheduled.append((process.name, quantum))
                     clock += quantum
-                    # print(scheduled[-1])
                 else:
-                    scheduled.append(
-                        (copy_list[i].name, copy_list[i].left_exec_time))
-                    clock += scheduled[-1][1]
+                    scheduled.append((process.name, process.left_exec_time))
+                    quantum = scheduled[-1][1]
+                    clock += quantum
                     print(scheduled[-1], f'-> popped at timestamp {clock}')
                     sum_finish_time += clock
-                    copy_list[i].left_exec_time = 0
+                    process.left_exec_time = 0
 
             copy_list = [x for x in copy_list if x.left_exec_time > 0]
 
         ave_waiting_time = sum_finish_time / len(self.rr_list)
         print('Average waiting time:', ave_waiting_time)
+
+        print(scheduled)
         return ave_waiting_time
 
     def plot(self):
