df14810cf720fb94749c0a22574440e5546c52fb
michisthr <michlstoehr19@icloud.com>
2024-12-19 10:59:18 +0100

    format rr.py

--- schedulers/rr.py
+++ schedulers/rr.py
@@ -1,24 +1,18 @@
 from process import RoundRobin_Process
 from plotter import Plotter
 
-def contains(list, filter):
-    for x in list:
-        if filter(x):
-            return True
-    return False
 
 class RoundRobin:
     def __init__(self):
         self.rr_list = [RoundRobin_Process(exec_time=22, name='P1'),
-           RoundRobin_Process(exec_time=2, name='P2'),
-           RoundRobin_Process(exec_time=3, name='P3'),
-           RoundRobin_Process(exec_time=5, name='P4'),
-           RoundRobin_Process(exec_time=8, name = 'P5')]
-
+                        RoundRobin_Process(exec_time=2, name='P2'),
+                        RoundRobin_Process(exec_time=3, name='P3'),
+                        RoundRobin_Process(exec_time=5, name='P4'),
+                        RoundRobin_Process(exec_time=8, name='P5')]
 
     def schedule(self) -> float:
         print("Round Robin:\n")
-        
+
         copy_list = self.rr_list.copy()
         quantum = int(input("Put in the quantum: "))
         print(f'\nCalculation for quantum {quantum}:')
@@ -30,23 +24,23 @@ class RoundRobin:
             for i in range(len_copy_list):
                 if (copy_list[i].left_exec_time > quantum):
                     copy_list[i].left_exec_time -= quantum
-                    scheduled.append((copy_list[i].name,quantum))
+                    scheduled.append((copy_list[i].name, quantum))
                     clock += quantum
                     # print(scheduled[-1])
                 else:
-                    scheduled.append((copy_list[i].name, copy_list[i].left_exec_time))
+                    scheduled.append(
+                        (copy_list[i].name, copy_list[i].left_exec_time))
                     clock += scheduled[-1][1]
                     print(scheduled[-1], f'-> popped at timestamp {clock}')
                     sum_finish_time += clock
                     copy_list[i].left_exec_time = 0
-                    
+
             copy_list = [x for x in copy_list if x.left_exec_time > 0]
 
         ave_waiting_time = sum_finish_time / len(self.rr_list)
         print('Average waiting time:', ave_waiting_time)
         return ave_waiting_time
-    
+
     def plot(self):
         self.plotter = Plotter(self.rr_list, title="Round Robin")
         self.plotter.plot()
-    
